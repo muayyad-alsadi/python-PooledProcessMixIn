8c3fd14bca474f8134f06a3e826c881089a10394
Muayyad Alsadi مؤيد السعدي <alsadi@ojuba.org>
2012-08-04 02:19:34 +0300

    update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python
 
+# review classifiers http://pypi.python.org/pypi?%3Aaction=list_classifiers
+
 import sys
 import os
 from distutils.core import setup
@@ -14,11 +16,12 @@ setup(name='PooledProcessMixIn',
       author_email='alsadi@gmail.com',
       url='https://github.com/muayyad-alsadi/python-PooledProcessMixIn',
       py_modules=['PooledProcessMixIn'],
+      data_files=[('', ('README.md','TODO')), ('demos', ('demos/demo.py', 'demos/wsgi-demo.py'))],
       license='PSFL',
       platforms = 'any',
       classifiers=['Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: PSF License',
+        'License :: OSI Approved :: Python Software Foundation License',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content :: CGI Tools/Libraries',
         'Topic :: Internet :: WWW/HTTP :: HTTP Servers',
         'Topic :: Internet :: WWW/HTTP :: WSGI :: Server',
